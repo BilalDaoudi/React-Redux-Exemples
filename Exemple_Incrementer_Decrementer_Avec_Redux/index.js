ab4c22fc5caebde5ff348e4a46708c2848b10f8a
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-09 12:21:40 +0100

    Update index.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/index.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/index.js
@@ -9,6 +9,7 @@ const store = legacy_createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  
   <Provider store={store}>
     <App />
   </Provider>
