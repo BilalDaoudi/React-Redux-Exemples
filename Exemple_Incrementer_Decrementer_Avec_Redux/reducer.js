d9a48fe726bd18ed32abb4cbee97bfd2cbb44a85
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-09 12:22:04 +0100

    Update reducer.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/reducer.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/reducer.js
@@ -1,14 +1,20 @@
 const initialeState = {num:0}
+
 const reducer = (state = initialeState, action) => {
+    
     switch(action.type) {
+            
         case 'Incrementer':
-            return {...state, num:state.num+1}
+            return {...state, num:state.num+1};
+            
         case 'Decrementer':
-            return {...state, num:state.num-1}
+            return {...state, num:state.num-1};
+            
         case 'Reset':
-            return {...state, num:0}
+            return {...state, num:0};
+            
         default:
-            return state
+            return state;
     }
 }
 
