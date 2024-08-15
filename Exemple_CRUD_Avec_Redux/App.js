5ef3d2a4fdc49d3a14b1ad4df7600f5276ad914c
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-15 10:02:56 +0100

    Update App.js

--- Exemple_CRUD_Avec_Redux/App.js
+++ Exemple_CRUD_Avec_Redux/App.js
@@ -5,7 +5,6 @@ import ListePersonne from './ListePersonne';
 import ModifierPersonne from './ModifierPersonne';
 
 const App = () => {
-    
     return (    
         <BrowserRouter> 
             <Routes>
@@ -15,7 +14,7 @@ const App = () => {
             </Routes>
         </BrowserRouter>
     );
-    
 };
+
 
 export default App;
