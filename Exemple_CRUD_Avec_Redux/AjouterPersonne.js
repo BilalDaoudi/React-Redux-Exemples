67b38107d9ba8df56c3afc49393a424966e0318f
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-15 10:02:42 +0100

    Update AjouterPersonne.js

--- Exemple_CRUD_Avec_Redux/AjouterPersonne.js
+++ Exemple_CRUD_Avec_Redux/AjouterPersonne.js
@@ -3,16 +3,14 @@ import {useSelector, useDispatch } from 'react-redux';
 import { Ajouter } from './actions';
 import { Link } from 'react-router-dom';    
 
-
 const AjouterPersonne = () => {
   const [nom, setNom] = useState('');
   const [prenom, setPrenom] = useState('');
   const [age, setAge] = useState('');
-
   const Personnes = useSelector((state) => state.Personnes);
   const newid = Personnes.length === 0 ? 1 : Personnes[Personnes.length-1].id + 1;
   const dispatch = useDispatch();
-  
+
   const AjouterPer = () => {
     const person = {id: newid, nom,prenom,age};
     dispatch(Ajouter(person));
@@ -29,7 +27,10 @@ const AjouterPersonne = () => {
       <Link onClick={AjouterPer} to="/"><button >Add Person</button></Link>
     </div>
   );
+
 };
+
+
 export default AjouterPersonne;
 
 
