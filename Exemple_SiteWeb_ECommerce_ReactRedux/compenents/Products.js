5254bd1224d9d9b654500fc97922ea5cca58ea6f
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-20 10:11:44 +0100

    Update Products.js

--- Exemple_SiteWeb_ECommerce_ReactRedux/compenents/Products.js
+++ Exemple_SiteWeb_ECommerce_ReactRedux/compenents/Products.js
@@ -1,11 +1,11 @@
 import React from "react";
+
 import { useSelector } from "react-redux";
+
 import Product from "./Product";
 
 export default function Products() {
-  
   const Products = useSelector(state => state.Products);
- 
   return (
     <div className="row">
       {Products.map((p, i) => (
