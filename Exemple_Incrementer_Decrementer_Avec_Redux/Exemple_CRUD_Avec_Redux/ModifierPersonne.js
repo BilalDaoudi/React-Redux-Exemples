f2ff10018fb6316acfac7613882ee87fffb518e7
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-16 10:35:06 +0100

    Update ModifierPersonne.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/ModifierPersonne.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/ModifierPersonne.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+
 import { useSelector, useDispatch } from "react-redux";
+
 import { useParams } from "react-router-dom";
+
 import { Modifier } from "./actions";
+
 import { Link } from "react-router-dom";
 
 const ModifierPersonne = () => {
