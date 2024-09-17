4f6374dceed45726dad116c6ba47846bfb03cff5
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-17 11:39:36 +0100

    Update index.js

--- Exemple_SiteWeb_ECommerce_ReactRedux/index.js
+++ Exemple_SiteWeb_ECommerce_ReactRedux/index.js
@@ -7,8 +7,10 @@ import "./style/style.css";
 import { legacy_createStore } from 'redux';
 import reducer from './config/reducer';
 
+
 const store = legacy_createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(
     <Provider store={store}>
