c2a3e8d3b21a5be1c1798b3a70b6b86c8746aa5e
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-17 11:36:38 +0100

    Update actions.js

--- Exemple_SiteWeb_ECommerce_ReactRedux/config/actions.js
+++ Exemple_SiteWeb_ECommerce_ReactRedux/config/actions.js
@@ -1,21 +1,15 @@
 export const DeleteAll = () => ({
     type: 'DeleteAll'
   });
-  
- 
   export const Add = (newcart) => ({
     type: 'Add',
     cart:newcart
   });
-  
   export const Delete = (id) => ({
     type: 'Delete',
     idcart:id
   });
-  
-    
   export const Update = (updatecart) => ({
     type: 'Update',
     cart:updatecart
   });
-  
