c8ddc2f7a48ea8debe74b1713e465d563089ee03
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-23 18:50:11 +0100

    Update index.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_Hooks_useState_useEffect/index.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_Hooks_useState_useEffect/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import ReactDOM from "react-dom/client";
+
 import App from './Exemple/App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<App header="Bilal né le 09/12/2002" btn="Nouzha" />);
