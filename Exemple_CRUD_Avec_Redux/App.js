82d252c91fd3159cea8167fcf4b06a17b9128ced
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-30 19:03:10 +0100

    Update App.js

--- Exemple_CRUD_Avec_Redux/App.js
+++ Exemple_CRUD_Avec_Redux/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
+
+
 import AjouterPersonne from './AjouterPersonne';
+
+
 import ListePersonne from './ListePersonne';
+
+
 import ModifierPersonne from './ModifierPersonne';
 
 const App = () => {
