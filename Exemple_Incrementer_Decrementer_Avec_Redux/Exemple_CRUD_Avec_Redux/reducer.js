af06c16713b3cba03802c561c66351e6c110a63b
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-08 18:01:41 +0100

    Update reducer.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/reducer.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/reducer.js
@@ -2,13 +2,18 @@
 const initialState = {
     Personnes: [],
   };
-  
+
+
   const reducer = (state = initialState, action) => {
     switch (action.type) {
       case 'Ajouter':
+            
         return {...state,Personnes: [...state.Personnes, action.payload],};
+            
       case 'Supprimer':
+            
         return {...state,Personnes: state.Personnes.filter((person) => person.id !== action.payload),};
+            
       case 'Modifier':
         return {...state,Personnes: state.Personnes.map((person) =>  person.id === action.payload.id ? action.payload : person),};
       default:
