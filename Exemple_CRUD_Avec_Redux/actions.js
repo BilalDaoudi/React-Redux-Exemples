62c2ad2f82969a2a0407dc31d600b88a319b7780
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-19 00:12:27 +0100

    Update actions.js

--- Exemple_CRUD_Avec_Redux/actions.js
+++ Exemple_CRUD_Avec_Redux/actions.js
@@ -1,12 +1,15 @@
 export const Ajouter = (personne) => ({
         type: 'Ajouter',
+        
         payload: personne,
       });
       export const Supprimer = (id) => ({
         type: 'Supprimer',
+              
         payload: id,
       });
       export const Modifier = (personne) => ({
         type: 'Modifier',
+              
         payload: personne,
       });
