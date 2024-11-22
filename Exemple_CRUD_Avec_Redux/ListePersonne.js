9d94735fe53661f78d7431e94a2e90edf455e17f
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-22 21:05:22 +0100

    Update ListePersonne.js

--- Exemple_CRUD_Avec_Redux/ListePersonne.js
+++ Exemple_CRUD_Avec_Redux/ListePersonne.js
@@ -1,13 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Supprimer } from "./actions";
 
-
 import { Link } from "react-router-dom";
-
-
-
-
-
 const ListePersonne = () => {
   const Personnes = useSelector((state) => state.Personnes);
   const dispatch = useDispatch();
