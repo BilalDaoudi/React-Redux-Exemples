32bfd62a3dd57a44952ffc4a50b3dfc037b2f8f0
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-12-23 20:11:27 +0100

    Update index.js

--- Exemple_CRUD_Avec_Redux/index.js
+++ Exemple_CRUD_Avec_Redux/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+
 import { Provider } from 'react-redux';
+
 import { legacy_createStore } from 'redux';
+
 import App from './App';
+
 import reducer from './reducer';
 
 const store = legacy_createStore(reducer);
