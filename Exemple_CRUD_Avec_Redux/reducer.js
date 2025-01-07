4b892cfd643225ef0d80ee5aeefccf28bae0c77f
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-01-07 23:56:39 +0100

    Update reducer.js

--- Exemple_CRUD_Avec_Redux/reducer.js
+++ Exemple_CRUD_Avec_Redux/reducer.js
@@ -4,12 +4,23 @@
 
   const reducer = (state = initialState, action) => {      
     switch (action.type) {
+
+            
       case 'Ajouter':            
-        return {...state,Personnes: [...state.Personnes, action.payload],};            
+        return {...state,Personnes: [...state.Personnes, action.payload],};     
+
+
+            
       case 'Supprimer':
-        return {...state,Personnes: state.Personnes.filter((person) => person.id !== action.payload),};              
+        return {...state,Personnes: state.Personnes.filter((person) => person.id !== action.payload),};    
+
+
+            
       case 'Modifier':
-        return {...state,Personnes: state.Personnes.map((person) =>  person.id === action.payload.id ? action.payload : person),};           
+        return {...state,Personnes: state.Personnes.map((person) =>  person.id === action.payload.id ? action.payload : person),}; 
+
+
+            
       default:
         return state;                    
     }  
