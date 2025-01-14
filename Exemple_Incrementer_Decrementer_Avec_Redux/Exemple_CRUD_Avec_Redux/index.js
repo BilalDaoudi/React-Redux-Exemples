116d9f3918bb9af399d2dca43f028014714d47e3
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-01-14 16:40:05 +0100

    Update index.js

--- Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/index.js
+++ Exemple_Incrementer_Decrementer_Avec_Redux/Exemple_CRUD_Avec_Redux/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import { legacy_createStore } from 'redux';
-
 import App from './App';
 import reducer from './reducer';
 const store = legacy_createStore(reducer)
@@ -13,34 +11,6 @@ root.render(
     <App />
   </Provider>
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
