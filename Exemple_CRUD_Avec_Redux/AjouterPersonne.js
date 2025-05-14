850a773ebfe2175c38abf36345fc9a4b418b1f09
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-14 14:10:40 +0000

    Update AjouterPersonne.js

--- Exemple_CRUD_Avec_Redux/AjouterPersonne.js
+++ Exemple_CRUD_Avec_Redux/AjouterPersonne.js
@@ -3,10 +3,6 @@ import {useSelector, useDispatch } from 'react-redux';
 import { Ajouter } from './actions';
 import { Link } from 'react-router-dom';    
 
-
-
-
-
 const AjouterPersonne = () => {
   const [nom, setNom] = useState('');
   const [prenom, setPrenom] = useState('');
